33111c6a4f42541cc268a2b34aaa8ef588d0438c
Mobin Ahmed <mobinahmed4321@gmail.com>
2024-04-01 04:29:25 -0700

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Apptycoons on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
